e6be06458c3c604bf403fb2745a2d7d1ec6f8507
m3use <77679025+musekorea@users.noreply.github.com>
2021-06-12 00:38:15 +0800

    localstorage

--- login.js
+++ login.js
@@ -1,9 +1,21 @@
 const loginForm = document.querySelector('#login-form');
-
 const userName = document.querySelector('#user-name');
+const greeting = document.querySelector('#greeting');
+const localStorageUserName = localStorage.getItem('username');
 
-loginForm.addEventListener('submit', function (event) {
+if (localStorageUserName === null) {
+  loginForm.classList.remove('hidden');
+  loginForm.addEventListener('submit', login);
+} else {
+  greeting.classList.remove('hidden');
+  greeting.textContent = `Hello ${localStorageUserName}`;
+}
+
+function login(event) {
   event.preventDefault();
   const nameValue = userName.value;
-  console.log(nameValue);
-});
+  loginForm.classList.add('hidden');
+  localStorage.setItem('username', nameValue);
+  greeting.classList.remove('hidden');
+  greeting.textContent = `Hello ${nameValue}`;
+}
