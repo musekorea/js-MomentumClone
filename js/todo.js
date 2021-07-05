385b2a1cdfe45394bb3f705fbe20bda1f3f8597e
m3use <77679025+musekorea@users.noreply.github.com>
2021-07-05 22:12:20 +0800

    add todo

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,21 @@
 const todoForm = document.querySelector('#todo-form');
 const todoList = document.querySelector('#todo-list');
 const todoInput = todoForm.querySelector('#todo-input');
-console.log(todoForm, todoList);
+
+function addTodo(newTodo) {
+  console.log(newTodo);
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  li.appendChild(span);
+  span.innerText = newTodo;
+  todoList.appendChild(li);
+}
+
+function delTodo() {}
 
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = ``;
-  console.log(newTodo);
+  addTodo(newTodo);
 });
