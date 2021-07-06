b495084156fcef7e1cd246ae8879e119e6560e01
m3use <77679025+musekorea@users.noreply.github.com>
2021-07-07 00:01:53 +0800

    save Todos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,8 @@ const todoForm = document.querySelector('#todo-form');
 const todoList = document.querySelector('#todo-list');
 const todoInput = todoForm.querySelector('#todo-input');
 
+let todos = [];
+
 function deleteTodo(e) {
   const li = e.target.parentNode;
   li.remove();
@@ -19,9 +21,23 @@ function addTodo(newTodo) {
   btn.addEventListener('click', deleteTodo);
 }
 
+function saveTodo(newTodo) {
+  console.log(todos);
+  localStorage.setItem('todos', todos);
+}
+
+function loadTodo() {
+  const localTodos = localStorage.getItem('todos');
+  console.log(localTodos);
+}
+
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = ``;
   addTodo(newTodo);
+  todos.push(newTodo);
+  saveTodo(newTodo);
 });
+
+loadTodo();
